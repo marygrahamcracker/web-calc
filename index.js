6b4092d4def7aef262e8f1f168b0ead2fa199dc9
Mary Graham <mary.katherine.graham@outlook.com>
2023-01-02 18:06:33 -0800

    got initial layout to work

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+let currentValue = 0;
+
 // on page load write a secret message to the browser console
 document.addEventListener('DOMContentLoaded', () => {
     console.log('secret message');
+    document.getElementById('txtResults').value = currentValue.toString();
 });
 
-const clickme = (c) => {
-    // alert('you can\'t type that');
+// const clickme = (c) => {
+//     // alert('you can\'t type that');
     
-    for (let i = 1; i <= 5; i++) {
-    lognumbers(i, 12);
-    }
-};
+//     for (let i = 1; i <= 5; i++) {
+//     lognumbers(i, 12);
+//     }
+// };
 
-const lognumbers = (multiplier, maxMultiplier) => {
-    let x = '';
-   for (let m = 1; m <= maxMultiplier; m++) {
-       let b = multiplier * m;
-       x = x + b + ', ';
-   }
-   console.log(x); 
-};
+// const lognumbers = (multiplier, maxMultiplier) => {
+//     let x = '';
+//    for (let m = 1; m <= maxMultiplier; m++) {
+//        let b = multiplier * m;
+//        x = x + b + ', ';
+//    }
+//    console.log(x); 
+// };
 
 
